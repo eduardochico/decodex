43047a9bf5972e31b54b1b9413fcef35f750533b
Eduardo Chico <eduardo.chico@gmail.com>
2025-06-19 13:42:12 -0600

    export LlmService

--- backend/src/scan/scan.module.ts
+++ backend/src/scan/scan.module.ts
@@ -5,9 +5,10 @@ import { ScanFile } from './scan-file.entity';
 import { ScanService } from './scan.service';
 import { ScanController } from './scan.controller';
 import { Application } from '../application/application.entity';
+import { LlmModule } from '../llm/llm.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Scan, ScanFile, Application])],
+  imports: [TypeOrmModule.forFeature([Scan, ScanFile, Application]), LlmModule],
   providers: [ScanService],
   controllers: [ScanController],
 })
